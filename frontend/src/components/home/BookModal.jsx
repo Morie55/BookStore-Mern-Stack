4642a126e0e74c1e1d967225119f7780b70be131
Morie Keita <kmorie18c@njala.edu.sl>
2024-10-26 09:23:59 +0000

    Added the config.js file to gitignore

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -1,4 +1,4 @@
-import { FaRegUserCircle } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 import { AiFillEdit } from "react-icons/ai";
 import { HiOutlineBookOpen } from "react-icons/hi";
 
@@ -12,7 +12,7 @@ const BookModal = ({ book, onClose }) => {
         onClick={(event) => event.stopPropagation()}
         className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
       >
-        <FaRegUserCircle
+        <IoMdClose
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
