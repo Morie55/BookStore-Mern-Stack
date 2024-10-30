8d5b5865a51bddad5b49a3d695284a56fab8ab77
Morie Keita <kmorie18c@njala.edu.sl>
2024-10-30 00:24:56 +0000

    Making the EditBook Responsive

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -56,9 +56,9 @@ const EditBook = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Edit Book</h1>
+      <h1 className="text-3xl my-4 text-center">Edit Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-full sm:w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
           <input
@@ -86,7 +86,10 @@ const EditBook = () => {
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+        <button
+          className="p-2 bg-sky-300 mt-4 sm:mt-8 w-full sm:w-auto mx-auto sm:mx-0"
+          onClick={handleEditBook}
+        >
           Save
         </button>
       </div>
