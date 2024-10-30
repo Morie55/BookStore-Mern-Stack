9fd449d0bba2a6bd3efa322274e4479c922bda99
Morie Keita <kmorie18c@njala.edu.sl>
2024-10-30 00:10:42 +0000

    Adding a responsive form layout.

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -37,9 +37,9 @@ const CreateBook = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Create Book</h1>
+      <h1 className="text-3xl my-4 text-center">Create Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-full sm:w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
           <input
@@ -67,7 +67,10 @@ const CreateBook = () => {
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+        <button
+          className="p-2 bg-sky-300 mt-4 sm:mt-8 w-full sm:w-auto mx-auto sm:mx-0"
+          onClick={handleSaveBook}
+        >
           Save
         </button>
       </div>
