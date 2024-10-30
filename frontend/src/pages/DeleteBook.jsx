07696ec0d9e332e133e0e671a870fcd08f489208
Morie Keita <kmorie18c@njala.edu.sl>
2024-10-30 00:34:19 +0000

    Making DeleteBook Responsive

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -31,13 +31,15 @@ const DeleteBook = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Delete Book </h1>
+      <h1 className="text-3xl my-4">Delete Book</h1>
       {loading ? <Spinner /> : ""}
 
-      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-        <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-full max-w-[600px] p-6 mx-4 sm:mx-auto">
+        <h3 className="text-xl sm:text-2xl text-center">
+          Are you sure you want to delete this book?
+        </h3>
         <button
-          className="p-4 bg-red-600 text-white m-8 w-full"
+          className="p-4 bg-red-600 text-white mt-6 w-full sm:w-auto sm:px-8 sm:mt-8"
           onClick={handleDeleteBook}
         >
           Yes, Delete it
